4ac1573b978ec95cfb987b37aebe51d834c0f0fa
Leonardo <leo.sn159@gmail.com>
2022-08-15 09:01:18 -0300

    ads personalizado

--- src/components/Ads/index.js
+++ src/components/Ads/index.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import SideAd from "../SideAd";
 import styles from "./Ads.module.css";
 
@@ -6,6 +7,9 @@ export const Ads = () => {
         <div className={styles.ads}>
             <div className={styles.container}>
                 <div className={styles.body}>
+                    <div className={styles.title}>
+                        <Image src="/favicon.ico" height={30} width={30}/><span>PokeAds | Click here to help us</span><Image src="/avatar.png" height={30} width={30}/>
+                    </div>
                     <SideAd />
                 </div>
             </div>
