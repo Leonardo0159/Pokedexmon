5df21a76e9f63ab50ffbfa7322ec327121a3d1f3
Leonardo <leo.sn159@gmail.com>
2022-09-06 16:15:26 -0300

    ad

--- src/components/SideAd/index.js
+++ src/components/SideAd/index.js
@@ -1,9 +1,6 @@
 import React, { useEffect } from "react"
-import { useState } from "react"
 
 const SideAd = () => {
-  const [teste, setTeste] = useState(1)
-
   useEffect(() => {
     const pushAd = () => {
       try {
@@ -19,7 +16,6 @@ const SideAd = () => {
       // Check if Adsense script is loaded every 300ms
       if (window.adsbygoogle) {
         pushAd()
-        console.log("teste")
         // clear the interval once the ad is pushed so that function isn't called indefinitely
         clearInterval(interval)
       }
@@ -31,18 +27,14 @@ const SideAd = () => {
   }, [])
 
   return (
-    <div>
-      <button onClick={() => {setTeste(teste+1)}}>teste</button>
-      <ins
-        className={"adsbygoogle"+teste}
-        style={{ display: "block" }}
-        data-ad-client="ca-pub-9645579603385719"
-        data-ad-slot="9441680405"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      >teste{teste}</ins>
-    </div>
-
+    <ins
+      className="adsbygoogle"
+      style={{ display: "block" }}
+      data-ad-client="ca-pub-9645579603385719"
+      data-ad-slot="9441680405"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    ></ins>
   )
 }
 
