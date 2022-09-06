6341f72ec6a74dfa9138b167be3a572610900cf0
Leonardo <leo.sn159@gmail.com>
2022-09-06 16:23:02 -0300

    Carousel Ad

--- src/components/Ads/index.js
+++ src/components/Ads/index.js
@@ -1,17 +1,34 @@
 import Image from "next/image";
 import SideAd from "../SideAd";
 import styles from "./Ads.module.css";
+import Carousel from 'react-bootstrap/Carousel';
+import { useState } from "react";
 
 export const Ads = () => {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
 
     return (
         <div className={styles.ads}>
             <div className={styles.container}>
                 <div className={styles.body}>
                     <div className={styles.title}>
-                        <Image src="/favicon.ico" height={30} width={30}/><span>PokeAds</span><Image src="/avatar.png" height={30} width={30}/>
+                        <Image src="/favicon.ico" height={30} width={30} /><span>PokeAds</span><Image src="/avatar.png" height={30} width={30} />
                     </div>
-                    <SideAd />
+                    <Carousel activeIndex={index} onSelect={handleSelect}>
+                        <Carousel.Item>
+                            <SideAd />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <SideAd />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <SideAd />
+                        </Carousel.Item>
+                    </Carousel>
                 </div>
             </div>
         </div>
